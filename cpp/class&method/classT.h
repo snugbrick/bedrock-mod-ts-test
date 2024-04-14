039028125f31e8b5d59f32858d0ad62bb1b58a0d
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-04-14 14:25:10 +0800

    _4m14d

--- cpp/class&method/classT.h
+++ cpp/class&method/classT.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #ifndef CLASS_LOANG
 #define CLASS_LOANG 10
@@ -7,7 +8,7 @@ using namespace std;
 
 class FirTry {
 private:
-  int money; // private by defalt
+  int _money; // private by defalt
   int check(int much) {
     if (much < 0) {
       cout << "y u much is a negative?" << endl;
@@ -17,6 +18,10 @@ private:
   }
 
 public:
+  FirTry(int money) { _money = money; }; // auto inline
+  ~FirTry() { std::cout << "bye" << std::endl; }
   void add(int much);
-  void print();
+  void print() { cout << _money << endl; };
+
+  const FirTry& maximun(const FirTry &ft2) const;
 };
--- cpp/class&method/classTry.cpp
+++ cpp/class&method/classTry.cpp
@@ -4,14 +4,25 @@
 using namespace std;
 
 int main() {
-  cout << "class test" << endl;
-  FirTry fir;
-  fir.add(-1);
-  fir.print();
+  {
+    cout << "class test" << endl;
+    FirTry fir = FirTry(-114);
+    FirTry fir2 = FirTry(-10);
+    // fir = fir2; // fir2 mumber to fir
+    fir.print();
+
+    FirTry newFir = fir.maximun(fir2);
+    newFir.print();
+  }
   return 0;
 }
 void FirTry::add(int much) {
   much = check(much);
-  money += much;
+  _money += much;
 }
-void FirTry::print() { cout << money << endl; }
+const FirTry &FirTry::maximun(const FirTry &ft2) const {
+  if (ft2._money > this->_money)
+    return ft2;
+  else
+    return *this;
+}
