e26585c1d29f24ddb7e24d456e89925b6e2379fe
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-05-01 07:38:05 +0800

    update

--- cppProject/src/main.cpp
+++ cppProject/src/main.cpp
@@ -8,9 +8,3 @@ int main() {
   std::cout << "Hello world!" << std::endl;
   printHi();
 }
-void printHi() {
-  using std::cout;
-  using std::endl;
-
-  cout << "Hi" << endl;
-}
--- cppProject/src/main.d
+++ cppProject/src/main.d
@@ -1,1 +1,2 @@
-src/main.o: src/main.cpp include/glad/glad.h include/KHR/khrplatform.h
+src/main.o: src/main.cpp include/glad/glad.h include/KHR/khrplatform.h \
+ include/GLFW/glfw3.h
